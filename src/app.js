c18ebb36cbe7a7c421a840a6719b676333f35ece
Rahul Pisal <rpisal@gmail.com>
2017-11-24 15:31:57 +0400

    code completed till lecture 102

--- src/app.js
+++ src/app.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 21000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', amount: 300, createdAt: 10000 }));
+
+store.dispatch(setTextFilter('bill'));
+
+//console.log(store.getState());
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
